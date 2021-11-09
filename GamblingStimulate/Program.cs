7407230da0b07cb939f0335cc90ce5c0617fb30d
NoorMdKhan123 <noormd6400@gmail.com>
2021-11-09 18:40:39 +0530

    Welcome to Gambling Program

---
+++ GamblingStimulate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamblingStimulate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambling Stimualte program");
+        }
+    }
+}
