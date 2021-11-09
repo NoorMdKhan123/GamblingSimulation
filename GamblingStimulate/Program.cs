f40d319f4a11b980b557ae9af98e39ca4822878d
NoorMdKhan123 <noormd6400@gmail.com>
2021-11-09 19:43:42 +0530

    Gambler started the game

--- GamblingStimulate/Program.cs
+++ GamblingStimulate/Program.cs
@@ -7,6 +7,8 @@ namespace GamblingStimulate
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambling Stimualte program");
+            StakeBet stakeBet = new StakeBet();
+            stakeBet.StartGame();
         }
     }
 }
